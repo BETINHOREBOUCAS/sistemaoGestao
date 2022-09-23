84a16516e0c4266f8079a37c1ce9bfdcc577ef09
Betinho Rebouças <betinhoreboucas91@gmail.com>
2022-09-23 20:33:27 -0300

    Modal pronto pára ser manipulado!

--- public_html/assets/js/teste.js
+++ public_html/assets/js/teste.js
@@ -1,20 +1,10 @@
-function ajaxTeste() {
-    fetch('http://localhost/sistemaoGestao/public_html/client')
-  .then(response => response.text())
-  .then(json => console.log(json))
-}
-
 /*Pegando formulario*/
 var form = document.getElementById("formulario");
 
 async function ajax() {
 
-    /* Mandando formulario via post manipulando cada informação
-    let data = new FormData();
-    
-    //Adiciona na variael
-    data.append('title', 'bla bla bla');
-    
+    //mandando todo o formulario criado
+    let data = new FormData(form);  
     
     let req = await fetch('http://localhost/sistemaoGestao/public_html/collaborator', {
         method: 'POST',
@@ -22,20 +12,7 @@ async function ajax() {
         
     });
 
-    let json = await req.text();
-    */
-
-    /* mandando todo o formulario criado
-    let data = new FormData(form);
+    // Transforma em text a requisição
+    await req.text();
     
-    
-    
-    let req = await fetch('http://localhost/sistemaoGestao/public_html/collaborator', {
-        method: 'POST',
-        body: data
-        
-    });
-
-    let json = await req.text();
-    */
 }
